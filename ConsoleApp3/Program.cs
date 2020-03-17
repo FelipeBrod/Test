fbfe03f3e5fd7eb0ab7475329ef512c3e26f4d0a
Felipe <beraldo.felipe@Gmail.com>
2020-03-17 16:39:52 -0400

    added bm dia

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worfgdsfgdgld!");
+            Console.WriteLine("Hello !");
+            Console.WriteLine("bom dia!");
 
             Console.ReadLine();
         }
